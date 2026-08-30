422a97e6cbdb37c45e557459554dddf415403eae
yashwanth2203 <yashwanthbittu047@gmail.com>
2026-08-31 01:48:50 +0530

    Add screening progress page

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+
 import '../../pages/landing_page.dart';
 import '../../pages/login_page.dart';
 import '../../pages/registration_page.dart';
 import '../../pages/dashboard_page.dart';
+import '../../pages/screening_progress_page.dart';
 
 class AppRoutes {
   static const String landing = '/';
@@ -11,6 +13,7 @@ class AppRoutes {
   static const String pricing = '/pricing';
   static const String screening = '/screening';
   static const String dashboard = '/dashboard';
+  static const String screeningProgress = '/screening-progress';
 
   static Map<String, WidgetBuilder> get routes {
     return {
@@ -20,6 +23,7 @@ class AppRoutes {
       pricing: (context) => const LandingPage(),
       screening: (context) => const LandingPage(),
       dashboard: (context) => const DashboardPage(),
+      screeningProgress: (context) => const ScreeningProgressPage(),
     };
   }
 }
